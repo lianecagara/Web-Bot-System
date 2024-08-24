cb3ab2211506f9940600137f32daa08231ec9cee
Liane Cagara (LiANE) <150021684+lianecagara@users.noreply.github.com>
2024-08-24 09:26:55 +0000

    ....

--- webbot.js
+++ webbot.js
@@ -1,11 +1,22 @@
 import express from "express";
+import fs from "fs";
+import path from "path";
+
+class Webbot {
+  static async main(args) {
+    console.log(">", ...args);
+    app.get("/", (req, res) => {
+      res.send("Test...");
+    });
+
+    app.listen(3000, () => {
+      console.log("Server started on port 3000");
+    });
+  }
+  static async loadAllCommands() {}
+}
+global.Webbot = Webbot;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Test...");
-});
-
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
-});
+Webbot.main(process.argv);

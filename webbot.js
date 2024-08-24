96c167a923c9849e889eeeb64881e876aaa20708
Liane Cagara (LiANE) <150021684+lianecagara@users.noreply.github.com>
2024-08-24 09:41:11 +0000

    cool stuffs

--- webbot.js
+++ webbot.js
@@ -2,10 +2,13 @@ import express from "express";
 import fs from "fs";
 import path from "path";
 import gradient from "gradient-string";
+const pkg = require("./package.json");
 
 class Webbot {
   static async main(args) {
     const { logger } = this;
+    logger(`Starting **WEB BOT SYSTEM** v${pkg.version}`, "info");
+    logger(`Author: **${pkg.author}**`, "info");
     app.get("/", (req, res) => {
       res.send("Test...");
     });
@@ -17,8 +20,16 @@ class Webbot {
   static logger(...args) {
     const title = args.length === 1 ? "info" : args.pop();
     const { retro } = gradient;
-    process.stdout.write(`${retro(`[${String(title).toUpperCase()}] -`)} ${args.join(" ")}\n`);
+    process.stdout.write(
+      `${retro(`[${String(title).toUpperCase()}] -`)} ${args.join(" ")}\n`.replace(
+        /\*\*(.*?)\*\*/g,
+        (match, p1) => {
+          return gradient.pastel(p1);
+        },
+      ),
+    );
   }
+  static pkg = pkg;
   static async loadAllCommands() {}
 }
 global.Webbot = Webbot;

1ee18fad00b6720036de557f2cfb3e9a9fd84228
Liane Cagara (LiANE) <150021684+lianecagara@users.noreply.github.com>
2024-08-24 09:33:18 +0000

    logger func

--- webbot.js
+++ webbot.js
@@ -1,17 +1,23 @@
 import express from "express";
 import fs from "fs";
 import path from "path";
+import gradient from "gradient-string";
 
 class Webbot {
   static async main(args) {
-    console.log(">", ...args);
+    const { logger } = this;
     app.get("/", (req, res) => {
       res.send("Test...");
     });
 
     app.listen(3000, () => {
-      console.log("Server started on port 3000");
+      logger("Server started on port 3000", ":D", "express");
     });
+  }
+  static logger(...args) {
+    const title = args.length === 1 ? "info" : args.pop();
+    const { retro } = gradient;
+    process.stdout.write(`${retro(`[${String(title).toUpperCase()}] -`)} ${args.join(" ")}\n`);
   }
   static async loadAllCommands() {}
 }
